f7e418bf383d03e294a630220830c5fd1eb9b7bf
Jacob Clark <jacob.jh.clark@gmail.com>
2022-06-25 13:02:16 +0100

    Generate possibilities horizontal and vertical

--- pins.rkt
+++ pins.rkt
@@ -1,17 +1,13 @@
 #lang racket
 
-(define (keypad) (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 0)))
+(define ht #hash(
+            (0 . (list 1 2 3))
+            (1 . (list 4 5 6))
+            (2 . (list 7 8 9))
+            (3 . (list 0))))
 
-(define (up x)
-  (match x
-    [x #:when (> x 0) (- x 1)]
-    [x '()]))
-
-(define (down x)
-  (match x
-    [x #:when (< x 2) (+ x 1)]
-    [x '()]))
-
+(define (up x) (match x [x #:when (> x 0) (- x 1)] [x '()]))
+(define (down x) (match x [x #:when (< x 2) (+ x 1)] [x '()]))
 (define (perms x) (cons (up x) (down x)))
 
 (define (m x)
@@ -22,23 +18,30 @@
     [x #:when (and (>= x 2) (<= x 3)) 2]
     [x '()]))
 
+(define (pivot-around-column row pivot-points)
+  (map (lambda (number)
+         (list-ref (hash-ref ht number) (+ row 1)))
+       pivot-points))
+
+(define (pivot-around-row row pivot-points)
+  (map (lambda (number)
+         (list-ref (hash-ref ht row) (+ number 1)))
+       pivot-points))
+
 (define (number-to-pad-position num)
   (define num-as-num (string->number num))
   (define column (m (/ num-as-num 3)))
   (define row (- (- num-as-num (* (m (/ num-as-num 3)) 3)) 1))
-
-  ; we need to pivot around row and column using the permutations below
-  ; eg fix row and pivot the column then fix the column and pivot the row
-  ; 4 = 1 0 = column pivot 1 0 & 1 1 = row pivot 0 0 & 1 0 & 2 0
-  ; then convert these to the actual numbers
-  (list (flatten (cons column (perms column))) (cons row (flatten (perms row)))))
+  (define current-number (list-ref (hash-ref ht column) (+ row 1)))
+  (define columns (flatten (cons column (perms column))))
+  (define rows (flatten (cons row (flatten (perms row)))))
+  
+  (list (flatten (pivot-around-column row columns)) (pivot-around-row column rows)))
 
 (define (get-row num)
-  (map (lambda (num)
-        (number-to-pad-position num)
-  ) num))
+  (map (lambda (num) (number-to-pad-position num)) num))
 
 (define (run approxpin)
   (get-row (map string (string->list (number->string approxpin)))))
 
-(run 4)
+(run 46)
